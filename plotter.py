b21c381d993b41331133a73b6164832f0e0c46c2
Rokevh <kenaniah.rona@nhs.net>
2025-03-25 10:16:46 +0000

    "Plotter updated"

--- plotter.py
+++ plotter.py
@@ -29,21 +29,30 @@ axs = [ax1,ax2]
 ax1.plot(
     p_filter_df["Ordinal"],
     p_filter_df["Performance"],
+    zorder=10
 )
 
 ax2.plot(
     p_filter_df["Ordinal"],
     p_filter_df["TOTAL TREATED"],
     color="r",
+    zorder=10
 )
 ax2.plot(
     p_filter_df["Ordinal"],
     p_filter_df["WITHIN STANDARD"],
-    color="g"
+    color="g",
+    zorder=10
 )
+
+fill_kwargs = {
+    "alpha": 0.3,
+    "zorder": 1
+}
+
 ax2.set_ylim(None, None)
-ax2.fill_between(p_filter_df["Ordinal"], p_filter_df["WITHIN STANDARD"], color="g", alpha=0.5)
-ax2.fill_between(p_filter_df["Ordinal"], p_filter_df["WITHIN STANDARD"], p_filter_df["TOTAL TREATED"], color="r", alpha=0.5)
+ax2.fill_between(p_filter_df["Ordinal"], p_filter_df["WITHIN STANDARD"], color="g", **fill_kwargs)
+ax2.fill_between(p_filter_df["Ordinal"], p_filter_df["WITHIN STANDARD"], p_filter_df["TOTAL TREATED"], color="r", **fill_kwargs)
 
 for ax in axs:
     ax.xaxis.set_ticklabels(p_filter_df["PERIOD"])
@@ -52,6 +61,7 @@ for ax in axs:
 for ax in (ax1, ax2):
     for _, row in epoch_df.iterrows():
         if row["ORG CODE"] == plot_org and row["STANDARD"] == plot_std:
+            
             slope = row["slope"]
             start_date = row["start_date"]
             end_date = row["end_date"]
@@ -60,11 +70,23 @@ for ax in (ax1, ax2):
 
             date_range = p_filter_df.loc[
                 (p_filter_df["Ordinal"] >= start_date)
-                & (p_filter_df["Ordinal"] >= end_date)
+                & (p_filter_df["Ordinal"] <= end_date)
             ]["Ordinal"]
 
-            ax.axvspan((start_date-10), (end_date+10), color=tier_colors[tiering_status], alpha=0.3, hatch="/")
+            ax.axvspan(
+                (start_date-10), 
+                (end_date+10), 
+                color=tier_colors[tiering_status], 
+                zorder=2,
+                alpha=0.3
+            )
 
 
+            y_range = [slope * (x + intercept/slope) for x in date_range]
+            ax1.plot(date_range, y_range, color=tier_colors[tiering_status])
+
+
+# Define a function that displays all providers in one massive plot. Ideally, I'd
+# want this to be organised by how well 
 
 plt.show()
